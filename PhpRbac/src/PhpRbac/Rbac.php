a1caddd978ec4da78dc761fbe3234ed1490aa79f
James Van Lommel <james.vanlommel@grab.com>
2014-12-12 17:40:25 -0800

    Move RbacManager class to be main Rbac class.
    No need for previous shim class to help with PSR autoloading issues.

--- PhpRbac/src/PhpRbac/Rbac.php
+++ PhpRbac/src/PhpRbac/Rbac.php
@@ -1,41 +1,198 @@
 <?php
-namespace PhpRbac;
+namespace models;
 
 /**
- * Provide NIST Level 2 Standard Role Based Access Control functionality.
+ * RBAC Manager - has the actual RBAC business logic.
  *
- * Allows maintainable function-level access control for enterprises, small
- * applications, or frameworks.
+ * Documentation regarding Rbac Manager functionality.
  *
- * @see http://phprbac.net/index.php
- * @see https://www.owasp.org/index.php/OWASP_PHPRBAC_Project
- **/
+ * Rbac Manager: Provides NIST Level 2 Standard Hierarchical Role Based Access Control
+ *
+ * Has three members, Roles, Users and Permissions for specific operations
+ *
+ * @author jamesvl
+ * @author abiusx
+ * @version 2.0
+ */
 class Rbac
 {
-    private $mgr;
+    /** @var \models\PermissionManager */
+    public $Permissions;
+    /** @var \models\RoleManager */
+    public $Roles;
+    /** @var \models\UserManager */
+    public $Users;
 
-    public function __construct($settings)
+    /**
+     * Create this class and configure Manager classes with proper backend.
+     *
+     * You _must_ pass in a map of options with the following parameters:
+     *
+     * dmap - one of 'mysql', 'sqlite', or 'postgres', defined in /dmap
+     * db   - itself a map containing
+     *        - dsn: the full data source name to connect to the DB
+     *        OR
+     *        - server
+     *        - schema (if applicable, for Postgres only)
+     *        - port
+     * user - username to connect to the db
+     * pass - password used to db connection; may be null for sqlite
+     * prefix - the table prefix to help namespace tables
+     **/
+    function __construct($opts)
     {
-        $this->mgr = new models\RbacManager($settings);
+        $this->Permissions = new PermissionManager();
+        $this->Roles = new RoleManager();
+        $this->Users = new UserManager();
     }
 
-    public function assign($role, $permission)
+    /**
+     * Assign a role to a permission.
+     *
+     * @param string|integer Id, Title or Path of the Role
+     * @param string|integer Id, Title or Path of the Permission
+     * @return boolean       Indicates result was successful or not
+     */
+    function assign($Role, $Permission)
     {
-        return $this->mgr->assign($role, $permission);
+        if (is_numeric($Role)) {
+            $RoleID = $Role;
+        }
+        else {
+            if (substr($Role, 0, 1) == "/")
+                $RoleID = $this->Roles->pathId($Role);
+            else
+                $RoleID = $this->Roles->titleId($Role);
+        }
+
+        if (is_numeric($Permission)) {
+            $PermissionID = $Permission;
+        }
+        else {
+            if (substr($Permission, 0, 1) == "/")
+                $PermissionID = $this->Permissions->pathId($Permission);
+            else
+                $PermissionID = $this->Permissions->titleId($Permission);
+        }
+
+        return $this->Roles->assign($RoleID, $PermissionID);
     }
 
-    public function check($permission, $user_id)
+    /**
+     * Unassign a Role from a Permission.
+     *
+     * @param string|integer Id, Title or Path of the Role
+     * @param string|integer Id, Title or Path of the Permission
+     * @return boolean       Indicates result was successful or not
+     **/
+    function unassign($Role, $Permission)
     {
-        return $this->mgr->check($permission, $user_id);
+        if (is_numeric($Role)) {
+            $RoleID = $Role;
+        }
+        else {
+            if (substr($Role, 0, 1) == "/")
+                $RoleID = $this->Roles->pathId($Role);
+            else
+                $RoleID = $this->Roles->titleId($Role);
+        }
+
+        if (is_numeric($Permission)) {
+            $PermissionID = $Permission;
+        }
+        else {
+            if (substr($Permission, 0, 1) == "/")
+                $PermissionID = $this->Permissions->pathId($Permission);
+            else
+                $PermissionID = $this->Permissions->titleId($Permission);
+        }
+
+        return $this->Roles->unassign($RoleID, $PermissionID);
     }
 
-    public function enforce($permission, $user_id)
+    /**
+     * Checks whether a user has a permission or not.
+     *
+     * @param string|integer $Permission
+     *            You can provide a path like /some/permission, a title, or the
+     *            permission ID.
+     *            In case of ID, don't forget to provide integer (not a string
+     *            containing a number).
+     * @param string|integer   User ID of a user
+     *
+     * @throws exceptions\PermissionNotFoundException
+     * @throws exceptions\UserNotProvidedException
+     * @return boolean
+     */
+    function check($Permission, $UserID = null)
     {
-        return $this->mgr->enforce($permission, $user_id);
+        if ($UserID === null)
+            throw new \exceptions\UserNotProvidedException("\$UserID is a required argument.");
+
+        // convert permission to ID
+        if (is_numeric ($Permission)) {
+            $PermissionID = $Permission;
+        }
+        else {
+            if (substr($Permission, 0, 1) == "/")
+                $PermissionID = $this->Permissions->pathId($Permission);
+            else
+                $PermissionID = $this->Permissions->titleId($Permission);
+        }
+
+        // if invalid, throw exception
+        if ($PermissionID === null)
+            throw new exceptions\PermissionNotFoundException("The permission '{$Permission}' not found.");
+
+        return $this->User->check($UserID, $PermissionID);
     }
 
-    public function reset($ensure = false)
+    /**
+     * Enforce a permission on a user.
+     *
+     * Side effects: sends a 403 Forbidden header and instantly dies().
+     *
+     * This will abort any code called by your framework, to use with caution!
+    *
+    * @param string|integer   path or title or ID of permission
+    * @param integer          User id.
+    *
+     * @throws exceptions\UserNotProvidedException
+    */
+    function enforce($Permission, $UserID = null)
     {
-        return $this->mgr->reset($ensure);
+        if ($UserID === null)
+            throw new \exceptions\UserNotProvidedException("\$UserID is a required argument.");
+
+        if (!$this->check($Permission, $UserID)) {
+            header('HTTP/1.1 403 Forbidden');
+            die("<strong>Forbidden</strong>: You do not have permission to access this resource.");
+        }
+
+        return true;
+    }
+
+    /**
+    * Remove all roles, permissions and assignments in your database.
+    *
+    * Used for testing.
+    *
+    * @param boolean   Must set to true or throws error.
+    * @return boolean
+    */
+    function reset($Ensure = false)
+    {
+        if ($Ensure !== true) {
+            throw new \Exception("You must pass true to this function, otherwise it won't work.");
+            return;
+        }
+
+        $res = true;
+        $res = $res and $this->Roles->resetAssignments(true);
+        $res = $res and $this->Roles->reset(true);
+        $res = $res and $this->Permissions->reset(true);
+        $res = $res and $this->Users->resetAssignments(true);
+
+        return $res;
     }
 }
