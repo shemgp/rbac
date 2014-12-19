f758112e8bff4cf4226010418d75c869292cbfb6
James Van Lommel <james.vanlommel@grab.com>
2014-12-19 15:51:12 -0800

    Modify testing config file and bootrstrap process to use one file for all DBs.

--- PhpRbac/tests/bootstrap.php
+++ PhpRbac/tests/bootstrap.php
@@ -2,10 +2,15 @@
 require_once('./database/database.config');
 
 global $TEST_CFG;
-$TEST_CFG = $cfg;
+
+// global settings from phpunit_*.xml files
+if ($GLOBALS['DB_ADAPTER'] == 'pdo_sqlite')
+   $TEST_CFG = $cfg['sqlite'];
+elseif ($GLOBALS['DB_ADAPTER'] == 'pdo_mysql')
+   $TEST_CFG = $cfg['mysql'];
 
 // turn on all errors
 error_reporting(E_ALL);
 
-// autoloader
+// use Composer autoloader
 require dirname(__DIR__) . '/../../../autoload.php';
