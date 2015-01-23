bca510f47c5082318fd3dc093ac66f25cccf6101
James Van Lommel <james.vanlommel@grab.com>
2015-01-22 16:52:37 -0800

    Add back original tests for nested set tree databases.
    MySQL and sqlite output types are assumed.
    Tests modified to allow null descriptions, or to explicitly insert empty
    strings.

---
+++ tests/bootstrap-nst.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '/database/database.config';
+
+global $TEST_CFG;
+
+// global settings from phpunit_*.xml files
+if ($GLOBALS['DB_ADAPTER'] == 'pdo_sqlite')
+   $TEST_CFG = $cfg['sqlite'];
+elseif ($GLOBALS['DB_ADAPTER'] == 'pdo_mysql')
+   $TEST_CFG = $cfg['mysql'];
+
+// turn on all errors
+error_reporting(E_ALL);
+
+// use Composer autoloader
+$autoloader = require(__DIR__ . '/../../../autoload.php');
+$autoloader->addPsr4('PhpRbac\\tests\\', __DIR__ . '/src-nst');
